ae93f9d85842719666e58ac91da9d1ed23a7da21
Paula Montiel <p_dany_mr@hotmail.com>
2018-06-15 14:55:00 -0400

    subiendo cambios y reloj

--- src/main.js
+++
@@ -1,9 +0,0 @@
-/*progress [user.id]["Nombre del curso"].units
- console.log = user.id + "Introducción" + units;
- /*Object.entries(obj)=>[[key, value]]
- Object.value(obj)=>[value1, value2..]
- object.keys(obj)=>[key1, key2...]*/
-
- window.progress 
-     const progress []
- 
