6d03fd65c8a36b1dd5833ec3d32d6e1080a12769
Tamara Hinojosa Salinas <tamara.hi.sa@hotmail.com>
2018-06-21 17:03:34 -0400

    Se logra llamar la data users a pantalla

--- src/main.js
+++ src/main.js
@@ -1,16 +1,31 @@
 function btnSedes() {
+    document.getElementById("bg").style.display = "none";
+    document.getElementById("tabla").style.visibility = "visible";
     fetch("../../data/cohorts/lim-2018-03-pre-core-pw/users.json")
 .then (Response => Response.json())
 .then(data => {
-    console.log(data);
+    //console.log(data);
     tabla1(data)
 })
-} 
+ 
 
 function tabla1(data){
-    console.log(data)
+    //console.log(data)
+    document.getElementById("contenido").innerHTML = ""
+    for(let valor of data){
+       // console.log(valor)
+       document.getElementById("contenido").innerHTML += `
+       
+       <tr>
+            <th scope="row">${valor.name}</th>
+            <td>${valor.id}</td>
+            <td>${valor.signupCohort}</td>
+            <td>${valor.timezone}</td>
+          </tr>
+       `
+    }
 }
-
+}
 function btnAlumnas() {
     fetch("../../data/cohorts.json")
 .then (Response => Response.json())
