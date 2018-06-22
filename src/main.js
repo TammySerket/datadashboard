b980320e359d875afcd2dfb7629ac0ca22e3c112
Paula Montiel <p_dany_mr@hotmail.com>
2018-06-22 17:01:50 -0400

    agregando commits

--- src/main.js
+++ src/main.js
@@ -1,29 +1,3 @@
-window.onload const computeUsersStats= Promise.all([   //Ejecuta todas las llamadas de manera paralela
-    fetch("../data/cohorts/cohorts.json"),
-    fetch("../data/cohorts/lim-2018-03-pre-core-pw/progress.json"),
-    fetch("../data/cohorts/lim-2018-03-pre-core-pw/users.json")
-    
-]).then(
-    (responses)=>{   //Responde a todas las promesas
-        return Promise.all(responses.map((response)=>{
-           return response.json();                
-        }));                        
-    }
-).then((responseJsons)=>{ //Arreglo de respuestas en json
-    console.log(responseJsonss);
-     
-    /*
-     * Código que ocupa los jsons...
-     */
-}).catch(
-    (error)=>{ // Al menos una llamada falló
-
-    }
-);
-console.log(computeUsersStats);
-
-
-
 function mostrar(id){
     if(id=='bg'){
         document.getElementById('bg').style.display = "block";
@@ -82,9 +56,4 @@ const cohortsJson = "../../data/cohorts.json"
 let cohorts = "";
 const progressJson= "../../data/cohorts/lim-2018-03-pre-core-pw/progress.json"
 let progress = "";*/
-
-
-
-
-
 
