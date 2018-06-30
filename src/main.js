43e15cce914e3c8de02ffa726765fcce04c663e1
Tamara Hinojosa Salinas <tamara.hi.sa@hotmail.com>
2018-06-30 18:25:25 -0400

    se logra que solo muestre curso con informacion pero ahora no lee cohorts

--- src/main.js
+++ src/main.js
@@ -1,46 +1,48 @@
-window.onload = function() {
+window.onload = function () {
     requiereCohorts();
 };
-  // llamamos los cohorts
+// llamamos los cohorts
 async function requiereCohorts() {
     let cohorts = '';
     try {
-      const jsonCohorts = await fetch('../data/cohorts.json');
-      cohorts = await jsonCohorts.json();
+        const jsonCohorts = await fetch('../data/cohorts.json');
+        cohorts = await jsonCohorts.json();
     } catch (err) {
-      alert('no se pudieron cargar los datos' + err);
+        alert('no se pudieron cargar los datos' + err);
     }
     inputChange(cohorts);
 }
 inputChange = (cohorts) => {
-    // mostrar la lista de cohorts
-    const cohortsId = cohorts.map(cohort => cohort.id);
     const cohortList = document.getElementById('cursos');
-    cohortsId.forEach(function(item) {
-      const option = document.createElement('option');
-      option.innerText = item;
-      cohortList.appendChild(option);
-    });
-    // Escuchar eventos del dom y llamar la funcion cada vez que se cambie el filtro
-    document.getElementById('cursos').addEventListener('change', async function() {
-        const e = document.getElementById("cursos");
-        const selection = e.options[e.selectedIndex].text;
-        const jsonUsers = await fetch('../data/cohorts/'+selection+'/users.json');
-        const jsonProgress = await fetch('../data/cohorts/'+selection+'/progress.json');
-        alumnas = await jsonUsers.json();
-        progreso = await jsonProgress.json();
-        /*console.log(progreso[userId]);*/
-        poblarTabla(computeUsersStats(alumnas, progreso, cohorts));
-    });
+    const cohortsId = cohorts.filter(element => {
+        return element.id === "lim-2018-03-pre-core-pw";
+    })
+    const curso = cohortsId[0].id;
+    const option = document.createElement('option');
+    option.innerText = curso;
+    cohortList.appendChild(option)
+    
+};
+// Escuchar eventos del dom y llamar la funcion cada vez que se cambie el filtro
+document.getElementById('cursos').addEventListener('change', async function () {
+    const e = document.getElementById("cursos");
+    const selection = e.options[e.selectedIndex].text;
+    const jsonUsers = await fetch('../data/cohorts/' + selection + '/users.json');
+    const jsonProgress = await fetch('../data/cohorts/' + selection + '/progress.json');
+    alumnas = await jsonUsers.json();
+    progreso = await jsonProgress.json();
+    /*console.log(progreso[userId]);*/
+    poblarTabla(computeUsersStats(alumnas, progreso, cohorts));
+});
 
-}
+
 async function poblarTabla(users) {
     document.getElementById('students').innerText = '';
 
     // Variables de estadisticas de cohort en general
     let completitudTotalSum = 0, percentLecturasSum = 0,
-    percentQuizzSum = 0, percentExercisesSum = 0; completitudTotalUser = 0;
-  
+        percentQuizzSum = 0, percentExercisesSum = 0; completitudTotalUser = 0;
+
     for (let i = 0; i < users.length; i++) {
         const userList = document.getElementById('students');
         const newStudent = document.createElement('div');
@@ -64,7 +66,7 @@ async function poblarTabla(users) {
     function createElement(parent, element, text, clase) {
         const newElement = document.createElement(element);
         if (text === users.name) {
-          newElement.classList.add('col-12');
+            newElement.classList.add('col-12');
         }
         newElement.innerText = text;
         newElement.classList.add(clase);
@@ -72,17 +74,17 @@ async function poblarTabla(users) {
         newElement.classList.add('col-2');
         parent.appendChild(newElement);
     }
-    completitudTotalSum += completitudTotalUser ;
+    completitudTotalSum += completitudTotalUser;
     percentLecturasSum += users[i].stats.reads.percent;
     percentQuizzSum += users[i].stats.quizzes.percent;
     percentExercisesSum += users[i].stats.exercises.percent;
-    
+
 }
-function mostrar(id){
-    if(id=='bg'){
+function mostrar(id) {
+    if (id == 'bg') {
         document.getElementById('bg').style.display = "block";
         document.getElementById('bg2').style.display = "none";
-    }else if(id=='bg2'){
+    } else if (id == 'bg2') {
         document.getElementById('bg').style.display = "none";
         document.getElementById('bg2').style.display = "block";
     }
